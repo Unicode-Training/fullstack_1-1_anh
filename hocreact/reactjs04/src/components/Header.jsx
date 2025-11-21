8c456ebf806573dbb532e1a60dae29a5df6777f3
Hoang An <hoangan@fullstack.edu.vn>
2025-11-21 23:15:34 +0700

    Update code

--- hocreact/reactjs04/src/components/Header.jsx
+++ hocreact/reactjs04/src/components/Header.jsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 import LoginModal from "./LoginModal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Spinner } from "./ui/spinner";
 
 export default function Header() {
   const menus = [
@@ -29,6 +30,34 @@ export default function Header() {
     },
   ];
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const requestProfile = async () => {
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) {
+      return;
+    }
+    setLoading(true);
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/auth/profile`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    const data = await response.json();
+    setUser(data);
+    setLoading(false);
+  };
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    setUser(null);
+  };
+  useEffect(() => {
+    requestProfile();
+  }, []);
   return (
     <div className="max-w-[1200px] mx-auto flex items-center py-3">
       <div className="max-w-[200px]">
@@ -52,16 +81,31 @@ export default function Header() {
             </li>
           ))}
         </ul>
-        <Button
-          className="rounded-full cursor-pointer py-[7px] px-5 hover:bg-gray-600"
-          size={null}
-          onClick={() => setIsOpenModal(!isOpenModal)}
-        >
-          <img src="/images/user-icon.png" alt="" />
-          Login/Signup
-        </Button>
+        {isLoading ? (
+          <Spinner />
+        ) : user ? (
+          <div className="flex gap-2 items-center">
+            <p>Hi, {user.name}</p>
+            <Button onClick={handleLogout}>Logout</Button>
+          </div>
+        ) : (
+          <Button
+            className="rounded-full cursor-pointer py-[7px] px-5 hover:bg-gray-600"
+            size={null}
+            onClick={() => setIsOpenModal(!isOpenModal)}
+          >
+            <img src="/images/user-icon.png" alt="" />
+            Login/Signup
+          </Button>
+        )}
       </div>
-      <LoginModal open={isOpenModal} />
+      <LoginModal
+        open={isOpenModal}
+        onClose={() => {
+          setIsOpenModal(false);
+          requestProfile();
+        }}
+      />
     </div>
   );
 }
--- hocreact/reactjs04/src/components/LoginModal.jsx
+++ hocreact/reactjs04/src/components/LoginModal.jsx
@@ -6,20 +6,94 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-export default function LoginModal({ open }) {
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useState } from "react";
+export default function LoginModal({ open, onClose }) {
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+  const [message, setMessage] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const handleChangeValue = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleLogin = async (email, password) => {
+    setLoading(true);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      setMessage("Email hoặc mật khẩu không chính xác");
+    } else {
+      const { access_token, refresh_token } = await response.json();
+      localStorage.setItem("access_token", access_token);
+      localStorage.setItem("refresh_token", refresh_token);
+      onClose();
+    }
+    setLoading(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = form;
+    const errors = {};
+    if (!email) {
+      errors.email = "Email is required";
+    }
+    if (!password) {
+      errors.password = "Password is required";
+    }
+    setErrors(errors);
+    if (!Object.keys(errors).length) {
+      handleLogin(email, password);
+    }
+  };
+
   return (
-    <Dialog
-      open={open}
-      onOpenChange={(status) => {
-        console.log(status);
-      }}
-    >
+    <Dialog open={open} onOpenChange={onClose}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+          <DialogTitle></DialogTitle>
+          <DialogDescription asChild>
+            <div>
+              <h2 className="text-2xl text-center mb-3">Login</h2>
+
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <label className="font-bold mb-2">Email</label>
+                  <Input
+                    type="email"
+                    placeholder="Email..."
+                    name="email"
+                    onChange={handleChangeValue}
+                  />
+                  {errors.email && (
+                    <span className="block text-red-600 mt-2">
+                      {errors.email}
+                    </span>
+                  )}
+                </div>
+                <div className="mb-3">
+                  <label className="font-bold mb-2">Password</label>
+                  <Input
+                    type="password"
+                    placeholder="Password..."
+                    name="password"
+                    onChange={handleChangeValue}
+                  />
+                  {errors.password && (
+                    <span className="block text-red-600 mt-2">
+                      {errors.password}
+                    </span>
+                  )}
+                </div>
+                <Button>{isLoading ? "Loading..." : "Login"}</Button>
+                {message && <p className="text-red-600 py-2">{message}</p>}
+              </form>
+            </div>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
